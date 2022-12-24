2d4a09279433d757c3d75f35cd4fd4cf18412e4e
Vladislav Bachmackiy <astrax200469@gmail.com>
2022-12-24 11:24:12 +0300

    feat(sass): wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCss = require('mini-css-extract-plugin');
 const path = require('path');
+const sass = require('sass');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+  ],
+  module: {
+    rules: [{
+      test: /\.sass$/i,
+      use: [
+        miniCss.loader,
+        "css-loader",
+        {
+          loader: "sass-loader",
+          options: {
+            implementation: sass,
+          },
+        }
+      ],
+    }]
+  },
 };
